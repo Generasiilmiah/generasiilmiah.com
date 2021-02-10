fe75e4040583086c5deb2886209f8bfe65f72520
Asebodi <aditraharditya45@gmail.com>
2021-02-10 16:10:40 +0700

    File cleanup

---
+++ pages/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function index(props) {
+  return <div className="flex items-center justify-center h-screen">Test</div>;
+}
+
+index.propTypes = {};
+
+export default index;
