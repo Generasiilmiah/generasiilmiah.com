d716b2864d0b96dd1c9a2bf488ca808777d78d02
Asebodi <aditraharditya45@gmail.com>
2021-02-15 11:23:22 +0700

    Navlink for nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,46 +1,53 @@
-import Link from "next/link";
 import React from "react";
-// import { ReactComponent as Logo } from "/logo.svg";
+import Link from "../NavLink";
 
 function Nav(props) {
   return (
     <nav className="px-6 h-20 flex justify-between items-center md:container md:mx-auto">
-      <img src="/logo.svg" alt="Logo Generasi Ilmiah" className="h-12" />
+      <Link href="/">
+        <a>
+          <img src="/logo.svg" alt="Logo Generasi Ilmiah" className="h-12" />
+        </a>
+      </Link>
 
       <ul className="hidden md:block md:flex md:gap-4 lg:gap-12">
         <li>
-          <Link href="/">
-            <a className="font-bold">Beranda</a>
+          <Link activeClassName="nav-active" href="/">
+            <a className="text-gray-500">Beranda</a>
           </Link>
         </li>
         <li>
-          <Link href="/kelas">
+          <Link activeClassName="nav-active" href="/kelas">
             <a className="text-gray-500">Kelas</a>
           </Link>
         </li>
         <li>
-          <Link href="/about-us">
+          <Link activeClassName="nav-active" href="/about-us">
             <a className="text-gray-500">Tentang Kami</a>
           </Link>
         </li>
         <li>
-          <Link href="/testimonial">
+          <Link activeClassName="nav-active" href="/testimonial">
             <a className="text-gray-500">Testimonial</a>
           </Link>
         </li>
         <li>
-          <Link href="/blog">
+          <Link activeClassName="nav-active" href="/blog">
             <a className="text-gray-500">Blog</a>
           </Link>
         </li>
       </ul>
 
-      <div
-        className="rounded-xl ring-2 ring-gray-600 w-20 h-9 flex justify-center items-center hidden md:flex"
-        style={{ boxSizing: "border-box" }}
-      >
-        <p>Masuk</p>
-      </div>
+      <Link href="/kelas">
+        <a className="block">
+          <div
+            className="rounded-xl px-6 py-1.5 h-full mr-4 theme-bg text-white font-bold flex justify-center items-center"
+            style={{ boxSizing: "border-box" }}
+          >
+            <p>Masuk</p>
+          </div>
+        </a>
+      </Link>
 
       <div className="flex flex-col gap-1 md:hidden">
         <div className="nav-icon"></div>
