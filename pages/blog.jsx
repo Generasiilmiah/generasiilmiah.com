cb26166b7df5ffcbb851ef7777155fb739d85827
Asebodi <aditraharditya45@gmail.com>
2021-02-21 16:55:19 +0700

    Coming soon for blog and gitignore update

--- pages/blog.jsx
+++ pages/blog.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 import BlogHighlight from "../components/BlogHighlight";
 import BlogItem from "../components/BlogItem";
+import ComingSoon from "../components/ComingSoon";
 
 function blog(props) {
   return (
     <div className="px-6 pt-8 md:container md:mx-auto">
-      <BlogHighlight />
+      {/* <BlogHighlight />
 
       <div className="md:grid md:grid-cols-2 md:gap-8">
         <BlogItem />
         <BlogItem />
         <BlogItem />
-      </div>
+      </div> */}
+      <ComingSoon bgImg="/assets/img/coming-soon.jpg" />
     </div>
   );
 }
